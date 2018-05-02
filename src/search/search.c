3991b42da59eaf343db28bdbbae548597a47c75f
Simon Kindström <simon.kindstrom@gmail.com>
2018-05-02 14:18:43 +0200

    Fix forgetting argument to searchGpu

--- src/search/search.c
+++ src/search/search.c
@@ -1,12 +1,12 @@
 #include "search.h"
 
-extern int searchCpu();
-extern int searchGpu();
+extern int searchCpu(MatchFunction);
+extern int searchCpuEmulateGpu(MatchFunction);
+extern int searchGpu(MatchFunction);
 
 int search(MatchFunction onMatch) {
   if (SEARCH_WITH_GPU || HETEROGENEOUS_DESIGN) {
-    return searchGpu();
-  } else {
-    return searchCpu(onMatch);
+    return searchGpu(onMatch);
   }
+  return searchCpuEmulateGpu(onMatch);
 }
